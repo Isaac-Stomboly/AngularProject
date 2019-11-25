017538f3d033b672c8a16a8e2b7a9060f8f105af
STOMBOLY <is0903892@otc.edu>
2019-11-25 09:20:58 -0600

    Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductList } from './products/product-list.component';
 import { StarComponent } from './shared/star.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ProductList, StarComponent
+    AppComponent, ProductList, StarComponent,WelcomeComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(
+      [
+        {
+          path:'products', component: ProductList
+        }, {
+          path: 'welcome', component: WelcomeComponent
+        }]
+    )
   ],
   bootstrap: [AppComponent]
 })
